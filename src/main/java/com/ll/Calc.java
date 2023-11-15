c609140379e04c62ff273b03a4f533c1e9ebf85d
naraekwon86 <wde0530@gmail.com>
2023-11-15 22:41:40 +0900

    기초TDD

---
+++ src/main/java/com/ll/Calc.java
@@ -0,0 +1,37 @@
+package com.ll;
+
+public class Calc {
+    public static int run(String exp) {
+        if (exp.isBlank()) return 0;
+
+        exp = stripOuterParentheses(exp);
+
+        final String[] expBits = exp.split(" ");
+        final String sign = expBits[1];
+
+        final int num1 = Integer.parseInt(expBits[0]);
+        final int num2 = Integer.parseInt(expBits[2]);
+
+        final int rs = switch (sign) {
+            case "+":
+                yield num1 + num2;
+            case "-":
+                yield num1 - num2;
+            case "*":
+                yield num1 * num2;
+            default:
+                yield num1 / num2;
+
+        };
+        return rs;
+    }
+        private static String stripOuterParentheses(String exp){
+        if(exp.startsWith("(") && exp.endsWith(")")){
+            return exp.substring(1, exp.length() -1);
+            }
+            return exp;
+        }
+
+
+}
+
---
+++ src/test/java/com/ll/CalcTest.java
@@ -0,0 +1,71 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalcTest {
+    @Test
+    @DisplayName("Calc에 run 메서드는 int를 리턴한다")
+    void t1(){
+        int rs = Calc.run("");
+    }
+    @Test
+    @DisplayName("10 + 10 = 20")
+    void  t2(){
+        int rs = Calc.run("10 + 10");
+
+        assertThat(rs).isEqualTo(20);
+    }
+    @Test
+    @DisplayName("10 + 20 = 30")
+    void  t3(){
+        int rs = Calc.run("10 + 20");
+
+        assertThat(rs).isEqualTo(30);
+    }
+    @Test
+    @DisplayName("10 - 10 = 0")
+    void  t4(){
+        int rs = Calc.run("10 - 10");
+
+        assertThat(rs).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("10 - 5 = 5")
+    void  t5(){
+        int rs = Calc.run("10 - 5");
+
+        assertThat(rs).isEqualTo(5);
+    }
+    @Test
+    @DisplayName("10 * 5 = 50")
+    void  t6(){
+        int rs = Calc.run("10 * 5");
+
+        assertThat(rs).isEqualTo(50);
+    }
+    @Test
+    @DisplayName("10 / 3 = 3")
+    void  t7(){
+        int rs = Calc.run("10 / 3");
+
+        assertThat(rs).isEqualTo(3);
+    }
+    @Test
+    @DisplayName("10 / 5 = 2")
+    void  t8(){
+        int rs = Calc.run("10 / 5");
+
+        assertThat(rs).isEqualTo(2);
+    }
+    @Test
+    @DisplayName("(10 + 10) = 20")
+    void  t9(){
+        int rs = Calc.run("(10 + 10)");
+
+        assertThat(rs).isEqualTo(20);
+    }
+
+}
